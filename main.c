aff235b6a9240506106f049ebe8039193844833f
Malith Banuka <malithbanukasubasinghe@gmail.com>
2025-08-08 21:31:31 +0530

    add functionality to calculate determinaters

--- main.c
+++ main.c
@@ -15,10 +15,11 @@
 #define DIV_SCLR -41
 #define TRANSPOSE -5
 #define AJOINT -6
+#define DETERMINENT -7
+#define INVERSE -8
 
 void get_matrix(float matrix[][MAX_SIZE], int rows, int cols)
 {
-    printf("%d", rows);
     for (int row = 0; row < rows; row++)
     {
         for (int col = 0; col < cols; col++)
@@ -76,22 +77,27 @@ void print_matrix(float matrix[][MAX_SIZE], int rows, int cols)
 
 int get_operation(int matrice_n)
 {
-    int option, options = 8;
+    int option, options = 10;
 
     printf("------------------------------------------------");
 
     printf("\nSelect an operation\n");
     if (matrice_n >= 2)
     {
-        printf("\n1. Add");
-        printf("\n2. subtract");
-        printf("\n3. Multiply");
-        printf("\n4. Divid");
+        printf("\n01. Add");
+        printf("\n02. subtract");
+        printf("\n03. Multiply");
+        printf("\n04. Divid");
     }
-    printf("\n5. Multiply by a scalar");
-    printf("\n6. Divid by a scalar");
-    printf("\n7. Get Transpose");
-    printf("\n8. Get Ajoint");
+    else
+    {
+        printf("\n05. Multiply by a scalar");
+        printf("\n06. Divid by a scalar");
+        printf("\n07. Get Determinent");
+        printf("\n08. Get Inverse");
+        printf("\n09. Get Transpose");
+        printf("\n10. Get Ajoint");
+    }
 
     printf("\n\nEnter operation id ( %d to %d ): ", matrice_n >= 2 ? 1 : 5, options);
 
@@ -137,10 +143,20 @@ int get_operation(int matrice_n)
     }
     case 7:
     {
-        return TRANSPOSE;
+        return DETERMINENT;
         break;
     }
     case 8:
+    {
+        return INVERSE;
+        break;
+    }
+    case 9:
+    {
+        return TRANSPOSE;
+        break;
+    }
+    case 10:
     {
         return AJOINT;
         break;
@@ -150,59 +166,125 @@ int get_operation(int matrice_n)
     }
 }
 
-void add(float matrix_1[][MAX_SIZE], float matrix_2[][MAX_SIZE], float answer_matrix[][MAX_SIZE], int size)
+void calculate(float matrix_1[][MAX_SIZE], float matrix_2[][MAX_SIZE], float answer_matrix[][MAX_SIZE], int size, float (*func)(float val_1, float val_2))
 {
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            answer_matrix[i][j] = matrix_1[i][j] + matrix_2[i][j];
+            answer_matrix[i][j] = (*func)(matrix_1[i][j], matrix_2[i][j]);
         }
     }
 }
 
-void subtract(float matrix_1[][MAX_SIZE], float matrix_2[][MAX_SIZE], float answer_matrix[][MAX_SIZE], int size)
+void calculate_wscalar(float matrix_1[][MAX_SIZE], float scalar, float answer_matrix[][MAX_SIZE], int size, float (*func)(float val_1, float val_2))
 {
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            answer_matrix[i][j] = matrix_1[i][j] - matrix_2[i][j];
+            answer_matrix[i][j] = (*func)(matrix_1[i][j], scalar);
         }
     }
 }
 
-void multiply(float matrix_1[][MAX_SIZE], float matrix_2[][MAX_SIZE], float answer_matrix[][MAX_SIZE], int size)
+float add(float value_1, float value_2)
 {
+    return value_1 + value_2;
+}
+
+float subtract(float value_1, float value_2)
+{
+    return value_1 - value_2;
+}
+
+float scalar_multiply(float scalar, float idx_value)
+{
+    return idx_value * scalar;
+}
+
+void multiply(float matrix_1[][MAX_SIZE], float matrix_2[][MAX_SIZE], float answer_matrix[][MAX_SIZE], int rows, int cols)
+{
+
+    int col, row;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            col = 0;
+            row = 0;
+            while (col < cols)
+            {
+                answer_matrix[i][j] += matrix_1[i][col] * matrix_2[row][j];
+                col++;
+                row++;
+            }
+        }
+    }
+
+}
+
+float scalar_divid(float scalar, float idx_value)
+{
+    return idx_value / scalar;
+}
+
+float get_determinater(float matrix[][MAX_SIZE], int size)
+{
+
+    print_matrix(matrix, size, size);
+    
+    if(size == 1) {
+        return matrix[0][0];
+    }
+
+    if (size == 2)
+    {
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    }
+    
+    float det = 0.0;
+    float matrix_t[MAX_SIZE][MAX_SIZE];
+    int next_col, next_row, selected_row = 0;
+
+    for (int selected_col = 0; selected_col < size; selected_col++)
+    {
+        next_row = 0;
+        for(int i = 0; i < size; i++) {
+            if(selected_row == i) continue;
+            next_col = 0; 
+            for(int k = 0; k < size; k++) {
+                if(selected_col == k) continue;
+                matrix_t[next_row][next_col++] = matrix[i][k]; 
+            }
+            next_row++;   
+        }   
+        float sign = pow(-1, (double)(selected_row + selected_col));
+        det += sign * matrix[selected_row][selected_col] * get_determinater(matrix_t, size-1);         
+    }
+    
+    return det;
+}
+
+void invert(float matrix[][MAX_SIZE], float inverse_matrix[][MAX_SIZE], int size)
+{
+    float determinent = get_determinater(matrix, size);
+
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            answer_matrix[i][j] = matrix_1[i][j] * matrix_2[i][j];
+            inverse_matrix[i][j] = matrix[i][j] * (1 / determinent);
         }
     }
 }
 
 void divid(float matrix_1[][MAX_SIZE], float matrix_2[][MAX_SIZE], float answer_matrix[][MAX_SIZE], int size)
 {
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            answer_matrix[i][j] = matrix_1[i][j] * (1 / matrix_2[i][j]);
-        }
-    }
-}
-
-void scalar_multiply(float matrix_1[][MAX_SIZE], float scalar, float answer_matrix[][MAX_SIZE], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            answer_matrix[i][j] = matrix_1[i][j] * scalar;
-        }
-    }
+    float inverse_matrix[MAX_SIZE][MAX_SIZE];
+    invert(matrix_2, inverse_matrix, size);
+    multiply(matrix_1, inverse_matrix, answer_matrix, size, size);
 }
 
 int main()
@@ -217,11 +299,12 @@ int main()
     for (int i = 0; i < matrices_n; i++)
     {
         printf("------------------------------------------------");
-        printf("\nEnter Number of columns of matrix %d: ", i + 1);
-        scanf("%d", &cols[i]);
 
-        printf("Enter Number of rows matrix %d: ", i + 1);
+        printf("\nEnter Number of rows matrix %d: ", i + 1);
         scanf("%d", &rows[i]);
+
+        printf("Enter Number of columns of matrix %d: ", i + 1);
+        scanf("%d", &cols[i]);
 
         printf("------------------------------------------------");
 
@@ -240,7 +323,7 @@ int main()
 
     if (matrices_n == 1)
     {
-        if (operation != MUL_SCLR && operation != DIV_SCLR && operation != TRANSPOSE && operation != AJOINT)
+        if (operation == MUL || operation == DIV || operation == ADD || operation == SUB)
         {
             printf("\n(!) At least 2 matrices required");
             return -1;
@@ -254,119 +337,143 @@ int main()
 
     switch (operation)
     {
-        case ADD:
+    case ADD:
+    {
+        int col_c = cols[0], row_c = rows[0];
+
+        for (int i = 0; i < (matrices_n - 1); i++)
         {
-            dimension = rows[0];
-            for (int i = 0; i < (matrices_n - 1); i++)
+            if (i != 0)
             {
-                if (rows[i] != dimension)
+                if (rows[i] != row_c || cols[i] != col_c)
                 {
                     printf("\n(!) Same dimension matrices required to perform addition among them");
                     return -1;
                 }
-                else
-                {
-                    if (i == 0)
-                    {
-                        add(matrices[i], matrices[i + 1], answer_matrices[i], dimension);
-                    }
-                    else
-                    {
-                        add(answer_matrices[i - 1], matrices[i + 1], answer_matrices[i], dimension);
-                    }
-                }
             }
-            printf("\nAnswer: ");
-            print_matrix(answer_matrices[matrices_n - 2], dimension, dimension);
-            break;
+
+            if (i == 0)
+            {
+                calculate(matrices[i], matrices[i + 1], answer_matrices[i], dimension, *add);
+            }
+            else
+            {
+                calculate(answer_matrices[i - 1], matrices[i + 1], answer_matrices[i], dimension, *add);
+            }
         }
 
-        case SUB:
+        printf("\nAnswer: ");
+        print_matrix(answer_matrices[matrices_n - 2], dimension, dimension);
+        break;
+    }
+
+    case SUB:
+    {
+        int col_c = cols[0], row_c = rows[0];
+
+        for (int j = 0; j < (matrices_n - 1); j++)
         {
-            for (int j = 0; j < (matrices_n - 1); j++)
+            if (j != 0)
             {
-                dimension = rows[0];
-                if (rows[j] != dimension)
+                if (rows[j] != row_c || cols[j] != col_c)
                 {
-                    printf("\n(!) Same dimension matrices required to perform subtraction among them");
+                    printf("\n(!) Same dimension matrices required to perform addition among them");
                     return -1;
                 }
-                else
-                {
-                    if (j == 0)
-                    {
-                        subtract(matrices[j], matrices[j + 1], answer_matrices[j], dimension);
-                    }
-                    else
-                    {
-                        subtract(answer_matrices[j - 1], matrices[j + 1], answer_matrices[j], dimension);
-                    }
-                }
             }
-            printf("\nAnswer: ");
-            print_matrix(answer_matrices[matrices_n - 2], dimension, dimension);
-            break;
+
+            if (j == 0)
+            {
+                calculate(matrices[j], matrices[j + 1], answer_matrices[j], dimension, *subtract);
+            }
+            else
+            {
+                calculate(answer_matrices[j - 1], matrices[j + 1], answer_matrices[j], dimension, *subtract);
+            }
         }
 
-        case MUL:
+        printf("\nAnswer: ");
+        print_matrix(answer_matrices[matrices_n - 2], dimension, dimension);
+        break;
+    }
+
+    case MUL:
+    {
+        for (int j = 0; j < (matrices_n - 1); j++)
         {
-            for (int j = 0; j < (matrices_n - 1); j++)
+            if (j != 0)
             {
-                dimension = rows[0];
-                if (rows[j] != dimension)
+                if (rows[j] != cols[j - 1] || cols[j] != rows[j - 1])
                 {
-                    printf("\n(!) Same dimension matrices required to perform subtraction among them");
+                    printf("\n(!) Same dimension matrices required to perform addition among them");
                     return -1;
                 }
-                else
-                {
-                    if (j == 0)
-                    {
-                        multiply(matrices[j], matrices[j + 1], answer_matrices[j], dimension);
-                    }
-                    else
-                    {
-                        multiply(answer_matrices[j - 1], matrices[j + 1], answer_matrices[j], dimension);
-                    }
-                }
             }
-            printf("\nAnswer: ");
-            print_matrix(answer_matrices[matrices_n - 2], dimension, dimension);
-            break;
+
+            if (j == 0)
+            {
+                multiply(matrices[j], matrices[j + 1], answer_matrices[j], rows[0], cols[1]);
+            }
+            else
+            {
+                multiply(answer_matrices[j - 1], matrices[j + 1], answer_matrices[j], rows[0], cols[1]);
+            }
         }
 
-        case DIV:
+        printf("\nAnswer: ");
+        print_matrix(answer_matrices[matrices_n - 2], rows[0], cols[1]);
+        break;
+    }
+
+    case DIV:
+    {
+        float det = 0.0;
+        dimension = rows[0];
+
+        for (int j = 0; j < (matrices_n - 1); j++)
         {
-            for (int j = 0; j < (matrices_n - 1); j++)
+
+            if (rows[j] != dimension || cols[j] != dimension)
             {
-                dimension = rows[0];
-                if (rows[j] != dimension)
-                {
-                    printf("\n(!) Same dimension matrices required to perform subtraction among them");
-                    return -1;
-                }
-                else
-                {
-                    if (j == 0)
-                    {
-                        divid(matrices[j], matrices[j + 1], answer_matrices[j], dimension);
-                    }
-                    else
-                    {
-                        divid(answer_matrices[j - 1], matrices[j + 1], answer_matrices[j], dimension);
-                    }
-                }
+                printf("\n(!) Same dimension squre matrices required to perform subtraction among them");
+                return -1;
             }
-            printf("\nAnswer: ");
-            print_matrix(answer_matrices[matrices_n - 2], dimension, dimension);
-            break;
+
+            if (j == 0)
+            {
+                divid(matrices[j], matrices[j + 1], answer_matrices[j], dimension);
+            }
+            else
+            {
+                divid(answer_matrices[j - 1], matrices[j + 1], answer_matrices[j], dimension);
+            }
         }
-        case MUL_SCLR: {
-            scalar_multiply(matrices[0], scalar, answer_matrices[0], rows[0]);
-            printf("\nAnswer: ");
-            print_matrix(answer_matrices[0], rows[0], rows[0]);
-            break;
-        }
+
+        printf("\nAnswer: ");
+        print_matrix(answer_matrices[matrices_n - 2], dimension, dimension);
+        break;
+    }
+
+    case MUL_SCLR:
+    {
+        calculate_wscalar(matrices[0], scalar, answer_matrices[0], rows[0], *scalar_multiply);
+        printf("\nAnswer: ");
+        print_matrix(answer_matrices[0], rows[0], rows[0]);
+        break;
+    }
+
+    case DIV_SCLR:
+    {
+        calculate_wscalar(matrices[0], scalar, answer_matrices[0], rows[0], *scalar_divid);
+        printf("\nAnswer: ");
+        print_matrix(answer_matrices[0], rows[0], rows[0]);
+        break;
+    }
+    case DETERMINENT: 
+    {
+        printf("\nDeterminent value: %.2f", get_determinater(matrices[0], rows[0]));
+    }
+
     }
 
     printf("\n");
